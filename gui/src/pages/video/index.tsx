6e474edbf7c2930862a84cafc90923d7b17d8287
Stu Kabakoff <sakabako@gmail.com>
2020-11-28 12:31:27 -0500

    start of video

--- gui/src/pages/video/index.tsx
+++ gui/src/pages/video/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+
 import { Page } from '../../components/Page';
+import { useVideo } from './useVideo';
 
 type Props = {
-    handle: FileSystemFileHandle;
+    handle: FileSystemFileHandle | undefined;
 };
 
 export const VideoUI = (props: Props) => {
-    return <Page title="Video">Video</Page>;
+    const [{ url, type }] = useVideo(props.handle);
+    console.log(url);
+    return (
+        <Page title="Video">
+            {url !== '' ? (
+                <video
+                    controls
+                    autoPlay
+                    width="400"
+                    onError={console.log}
+                    onCanPlay={console.log}
+                    onLoadedMetadata={console.log}
+                    onInvalid={console.log}
+                >
+                    <source src={url} type={type} />
+                </video>
+            ) : null}
+        </Page>
+    );
 };
